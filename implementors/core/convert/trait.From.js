529c48b034a33cf9b9eeda8730e03e7a6c57b8e1
devashishdxt <devashishdxt@users.noreply.github.com>
2019-10-22 08:22:57 +0000

    Automated deployment: Tue Oct 22 08:22:57 UTC 2019 6d4766f8a3b385fdd7b9d952e0ba1ff3ea1f8700

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["abci"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"https://doc.rust-lang.org/nightly/std/net/addr/enum.SocketAddr.html\" title=\"enum std::net::addr::SocketAddr\">SocketAddr</a>&gt; for <a class=\"enum\" href=\"abci/enum.Address.html\" title=\"enum abci::Address\">Address</a>",synthetic:false,types:["abci::server::Address"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/path/struct.PathBuf.html\" title=\"struct std::path::PathBuf\">PathBuf</a>&gt; for <a class=\"enum\" href=\"abci/enum.Address.html\" title=\"enum abci::Address\">Address</a>",synthetic:false,types:["abci::server::Address"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
