29335f9294b5767be0d37eca4931155a7d6f4cda
devashishdxt <devashishdxt@users.noreply.github.com>
2020-07-28 06:42:24 +0000

    Automated deployment: Tue Jul 28 06:42:24 UTC 2020 2350040661002f54982b5dc17c36eafafdf05a0b

--- implementors/std/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["abci"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"abci/types/struct.Error.html\" title=\"struct abci::types::Error\">Error</a>","synthetic":false,"types":["abci::types::Error"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
