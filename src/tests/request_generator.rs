99fc89e472969d24d5c12a3a4dbb7f61243ceb7e
Devashish Dixit <devashishdxt@gmail.com>
2021-10-21 11:56:16 +0800

    Fix clippy warnings

--- src/tests/request_generator.rs
+++ src/tests/request_generator.rs
@@ -3,68 +3,71 @@ use tendermint_proto::abci::{request::Value as RequestValue, Request};
 use crate::types::*;
 
 pub fn info() -> Request {
-    let mut request = Request::default();
-    request.value = Some(RequestValue::Info(RequestInfo::default()));
-    request
+    Request {
+        value: Some(RequestValue::Info(RequestInfo::default())),
+    }
 }
 
 pub fn init_chain() -> Request {
-    let mut request = Request::default();
-    request.value = Some(RequestValue::InitChain(Default::default()));
-    request
+    Request {
+        value: Some(RequestValue::InitChain(Default::default())),
+    }
 }
 
 pub fn begin_block(block_height: i64, app_hash: Vec<u8>) -> Request {
-    let mut begin_block_request = RequestBeginBlock::default();
+    let header = Header {
+        height: block_height,
+        app_hash,
+        ..Default::default()
+    };
 
-    let mut header = Header::default();
-    header.height = block_height;
-    header.app_hash = app_hash;
+    let begin_block_request = RequestBeginBlock {
+        header: Some(header),
+        ..Default::default()
+    };
 
-    begin_block_request.header = Some(header).into();
-
-    let mut request = Request::default();
-    request.value = Some(RequestValue::BeginBlock(begin_block_request));
-
-    request
+    Request {
+        value: Some(RequestValue::BeginBlock(begin_block_request)),
+    }
 }
 
 pub fn check_tx(counter: u64, recheck: bool) -> Request {
-    let mut check_tx_request = RequestCheckTx::default();
-    check_tx_request.tx = counter.to_be_bytes().to_vec();
+    let mut check_tx_request = RequestCheckTx {
+        tx: counter.to_be_bytes().to_vec(),
+        ..Default::default()
+    };
 
     if recheck {
         check_tx_request.set_type(CheckTxType::Recheck);
     }
 
-    let mut request = Request::default();
-    request.value = Some(RequestValue::CheckTx(check_tx_request));
-
-    request
+    Request {
+        value: Some(RequestValue::CheckTx(check_tx_request)),
+    }
 }
 
 pub fn deliver_tx(counter: u64) -> Request {
-    let mut deliver_tx_request = RequestDeliverTx::default();
-    deliver_tx_request.tx = counter.to_be_bytes().to_vec();
+    let deliver_tx_request = RequestDeliverTx {
+        tx: counter.to_be_bytes().to_vec(),
+    };
 
-    let mut request = Request::default();
-    request.value = Some(RequestValue::DeliverTx(deliver_tx_request));
-
-    request
+    Request {
+        value: Some(RequestValue::DeliverTx(deliver_tx_request)),
+    }
 }
 
 pub fn end_block(block_height: i64) -> Request {
-    let mut end_block_request = RequestEndBlock::default();
-    end_block_request.height = block_height;
+    let end_block_request = RequestEndBlock {
+        height: block_height,
+    };
 
-    let mut request = Request::default();
-    request.value = Some(RequestValue::EndBlock(end_block_request));
-
-    request
+    Request {
+        value: Some(RequestValue::EndBlock(end_block_request)),
+    }
 }
 
 pub fn commit() -> Request {
-    let mut request = Request::default();
-    request.value = Some(RequestValue::Commit(RequestCommit::default()));
-    request
+    Request {
+        value: Some(RequestValue::Commit(RequestCommit::default())),
+    }
 }
